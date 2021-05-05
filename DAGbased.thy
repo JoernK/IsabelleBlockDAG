a0d831a2233fee90ff1eb66e81d2030fe8d1c889
JoernK <joern.kussmaul@web.de>
2021-05-05 13:48:24 +0200

    Update DAGbased.thy

--- DAGbased.thy
+++ DAGbased.thy
@@ -43,38 +43,74 @@ qed
   
 definition (in DAGbased) reduce_past:: "'a \<Rightarrow> ('a,'b) pre_digraph"
   where 
-  "reduce_past a = \<lparr>verts = past_nodes a, 
-  arcs = {b. (b \<in> arcs G  \<and> ((head G b) \<in> past_nodes a) \<and> ((tail G b) \<in> past_nodes a)) }, tail = tail G, 
-  head = head G\<rparr>
- "
-  
+  "reduce_past a = induce_subgraph G (past_nodes a)"
+
+lemma (in DAGbased) reduce_past_subarcs:
+  "\<And>e. e \<in> arcs (reduce_past a) \<Longrightarrow> e \<in> arcs G"
+  using DAGbased_axioms_def past_nodes_def reduce_past_def by auto
+
+lemma (in DAGbased) reduce_past_path:
+  "\<And>u v. u \<rightarrow>\<^sup>+\<^bsub>reduce_past a\<^esub> v \<Longrightarrow> u \<rightarrow>\<^sup>+\<^bsub>G\<^esub> v"
+  using past_nodes_def reduce_past_def
+  by (smt (verit, ccfv_threshold) adj_not_same cycle_free 
+      dominates_induce_subgraphD reachable_adjI 
+      reachable_neq_reachable1 reachable_trans
+      rtrancl_trancl_trancl tranclD trancl_trans_induct)
+
 lemma (in DAGbased) reduce_dag:
   assumes " a \<in> verts G"
   assumes "\<not>genesis_node a"
   shows "DAGbased (reduce_past a)"
 proof 
-  have "finite (verts (reduce_past a))"
+  show "finite (verts (reduce_past a))"
     using DAGbased_axioms_def past_nodes_def reduce_past_def by auto
-  have "finite (arcs (reduce_past a))"
+  next
+  show "finite (arcs (reduce_past a))"
     using DAGbased_axioms_def past_nodes_def reduce_past_def by auto
-  have "\<And>e. e \<in> arcs (reduce_past a) \<Longrightarrow> tail (reduce_past a) e \<in> verts (reduce_past a)"
+  next
+  show "\<And>e. e \<in> arcs (reduce_past a) \<Longrightarrow> tail (reduce_past a) e \<in> verts (reduce_past a)"
     using DAGbased_axioms_def past_nodes_def reduce_past_def by auto
-  have "\<And>e. e \<in> arcs (reduce_past a) \<Longrightarrow> head (reduce_past a) e \<in> verts (reduce_past a)"
+  next
+  show "\<And>e. e \<in> arcs (reduce_past a) \<Longrightarrow> head (reduce_past a) e \<in> verts (reduce_past a)"
     using DAGbased_axioms_def past_nodes_def reduce_past_def by auto
-  have "\<And>e. e \<in> arcs (reduce_past a) \<Longrightarrow> e \<in> arcs G"
-    using DAGbased_axioms_def past_nodes_def reduce_past_def by auto
-  then have "\<And>e. e \<in> arcs (reduce_past a) \<Longrightarrow> tail (reduce_past a) e \<noteq> head (reduce_past a) e"
-    using past_nodes_def reduce_past_def
-    by (metis (no_types, lifting) no_loops pre_digraph.ext_inject pre_digraph.surjective) 
-  have " \<And>e1 e2.
+  next
+  show "\<And>e. e \<in> arcs (reduce_past a) \<Longrightarrow> tail (reduce_past a) e \<noteq> head (reduce_past a) e"
+    using past_nodes_def reduce_past_def reduce_past_subarcs
+    by (metis induce_subgraph_head induce_subgraph_tail no_loops) 
+  next
+  show "\<And>e1 e2.
        e1 \<in> arcs (reduce_past a) \<Longrightarrow>
-       e2 \<in> arcs (reduce_past a) \<Longrightarrow> arc_to_ends (reduce_past a) e1 = arc_to_ends (reduce_past a) e2 \<Longrightarrow> e1 = e2"
-    using past_nodes_def reduce_past_def
-    by (metis (no_types, lifting) \<open>\<And>e. e \<in> arcs (reduce_past a) \<Longrightarrow> e \<in> arcs G\<close>
-        arc_to_ends_def no_multi_arcs pre_digraph.select_convs(3) pre_digraph.select_convs(4)) 
-  have " \<And>u v. u \<rightarrow>\<^sup>+\<^bsub>reduce_past a\<^esub> v \<longrightarrow> \<not> v \<rightarrow>\<^sup>*\<^bsub>reduce_past a\<^esub> u"
-    using digraphI_induced
-    oops
+       e2 \<in> arcs (reduce_past a) \<Longrightarrow>
+       arc_to_ends (reduce_past a) e1 = arc_to_ends (reduce_past a) e2 \<Longrightarrow> e1 = e2"
+    using past_nodes_def reduce_past_def reduce_past_subarcs
+     by (metis \<open>\<And>e. e \<in> arcs (reduce_past a) \<Longrightarrow> e \<in> arcs G\<close> induce_subgraph_ends no_multi_arcs)
+ next 
+   show "\<And>u v. u \<rightarrow>\<^sup>+\<^bsub>reduce_past a\<^esub> v \<longrightarrow> \<not> v \<rightarrow>\<^sup>*\<^bsub>reduce_past a\<^esub> u"
+   proof 
+     fix a u v 
+     assume "u \<rightarrow>\<^sup>+\<^bsub>reduce_past a\<^esub> v"
+       have "u \<rightarrow>\<^sup>+ v"
+         by (smt (z3) \<open>u \<rightarrow>\<^sup>+\<^bsub>reduce_past a\<^esub> v\<close> adj_in_verts(2) adj_not_same dominates_induce_subgraphD reachable_adjI reachable_conv' reachable_neq_reachable1 reachable_trans reduce_past_def rtrancl_trancl_trancl tranclD trancl_rtrancl_trancl trancl_trans_induct)
+       then have "\<not>v \<rightarrow>\<^sup>* u"
+         using cycle_free
+         by auto
+     then show "\<not> v \<rightarrow>\<^sup>*\<^bsub>reduce_past a\<^esub> u"
+       using reduce_past_path
+       by (meson \<open>u \<rightarrow>\<^sup>+\<^bsub>reduce_past a\<^esub> v\<close> cycle_free reachable1_in_verts(1)
+           reachable_refl reachable_rtranclI trancl_rtrancl_trancl)
+   qed
+ next
+   show "\<And>e u v.
+       wf_digraph.arc (reduce_past a) e (u, v) \<longrightarrow>
+       \<not> u \<rightarrow>\<^sup>*\<^bsub>pre_digraph.del_arc (reduce_past a) e\<^esub> v"
+   proof 
+     fix e u v
+     assume "wf_digraph.arc (reduce_past a) e (u, v)"
+     then show "\<not> u \<rightarrow>\<^sup>*\<^bsub>pre_digraph.del_arc (reduce_past a) e\<^esub> v"
+       oops        
+    
+   
+  
 definition (in tie_breakingDAG) simple_vote:: "'a \<Rightarrow> 'a \<Rightarrow> int \<Rightarrow> bool" 
   where "simple_vote a b i = (if i=0 then (tie_break a b) else (i > 0))"
  
